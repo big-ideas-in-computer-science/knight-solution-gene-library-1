c124367bccf1d1cd726e76c2d5e9c71ebbdad88a
pricilla <pricilla@gmail.com>
2018-08-07 15:41:40 +0200

    hello, added some docu

--- interface.py
+++ interface.py
@@ -1,16 +1,19 @@
+
 class IndividualGene:
+    #for the initial position chose a random start position
     def __init__(self, configuration):
         self.configuration = configuration
-
+#mutation is that the line will be maintained until a random postition and randomized on from this point onwards
     def mutate(self):
         pass
-
+#crossover is the product of the replication of one line until the first shared position and replication the second line
+    #  from that moment on until the second shared number
     def crossover(self, other):
         pass
-
+#number of steps that follow the rule
     def fitness(self):
         pass
-
+#sequence of steps
     def path(self):
         pass
 
